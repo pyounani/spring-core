0729c0916840ae3cb00f2cf54e67de8b4d7a2815
pyounani <yndbsk9372@gmail.com>
2024-10-15 11:47:32 +0900

    [Feat] 회원 도메인과 관련된 클래스 구현

---
+++ src/main/java/pyounani/springCore/member/Member.java
@@ -0,0 +1,38 @@
+package pyounani.springCore.member;
+
+public class Member {
+
+    private Long id;
+    public String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+}
