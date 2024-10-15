57515c39cc9dc010194ed95c2b4c960c210523b6
pyounani <yndbsk9372@gmail.com>
2024-10-15 10:56:15 +0900

    [init] git init

---
+++ src/test/java/pyounani/springCore/SpringCoreApplicationTests.java
@@ -0,0 +1,11 @@
+package pyounani.springCore;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
