2dc37c1e049f9ef884e42b8c9c0d4f3a250c6d4f
pyounani <yndbsk9372@gmail.com>
2024-10-16 14:17:44 +0900

    [Refactor] AppConfig 리팩토링

--- src/main/java/pyounani/springCore/AppConfig.java
+++ src/main/java/pyounani/springCore/AppConfig.java
@@ -1,6 +1,8 @@
 package pyounani.springCore;
 
+import pyounani.springCore.discount.DiscountPolicy;
 import pyounani.springCore.discount.RateDiscountPolicy;
+import pyounani.springCore.member.MemberRepository;
 import pyounani.springCore.member.MemberService;
 import pyounani.springCore.member.MemberServiceImpl;
 import pyounani.springCore.member.MemoryMemberRepository;
@@ -10,13 +12,21 @@ import pyounani.springCore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new RateDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
