ab321b6dd6cfc3badac97c81d674bcbc683056c0
pyounani <yndbsk9372@gmail.com>
2024-10-18 16:11:19 +0900

    [Feat] 조회한 모든 빈이 필요한 경우 테스트

--- src/test/java/pyounani/springCore/autowired/AllBeanTest.java
+++ src/test/java/pyounani/springCore/autowired/AllBeanTest.java
@@ -1,4 +1,42 @@
 package pyounani.springCore.autowired;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pyounani.springCore.AutoAppConfig;
+import pyounani.springCore.discount.DiscountPolicy;
+import pyounani.springCore.member.Grade;
+import pyounani.springCore.member.Member;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    @RequiredArgsConstructor
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
 }
+
